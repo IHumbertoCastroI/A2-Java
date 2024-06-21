d721bb35d745aef5ba7ca8879e6b6044edd98cf0
IHumbertoCastroI <humberto.c.castro@gmail.com>
2024-06-21 16:28:47 -0300

    Main, Mensageiro, Sala-Teste
    Teste para a Class "Mensageiro", que pode ser utilizada dentro de qualquer método, e tem a  função de criar uma animação de "typing"
    
    ...Uma vibe muito daora

---
+++ Teste.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Teste {
+    
+    public void metodoQualquer() {
+        Scanner scanner = new Scanner(System.in);  // Inicializa o Scanner para ler da entrada padrão
+        int num1;
+        System.out.print("Procure em: \n\n 1 - para Balde \n 2 - para Baú \n 3 - para Cesto: \n\n");
+        
+        do {
+            num1 = scanner.nextInt();  // Ler um número inteiro
+
+            switch (num1) {
+                case 1:
+                    Mensageiro.exibirMensagem("Jason não encontra nada no Balde.\n",90);
+                    break;
+                case 2:
+                    Mensageiro.exibirMensagem("Jason não encontra nada no Baú.\n",90);
+                    break;
+                case 3:
+                    Mensageiro.exibirMensagem("...",500);
+                    Mensageiro.exibirMensagem("Miau Miaaauu",100);
+                    Mensageiro.exibirMensagem("...",500);
+                    System.out.println("\n");                    
+                    Mensageiro.exibirMensagem("░░░░░░░░░░░░░░░░░░░░░▄▀░░▌\r\n" + //
+                                                "░░░░░░░░░░░░░░░░░░░▄▀▐░░░▌\r\n" + //
+                                                "░░░░░░░░░░░░░░░░▄▀▀▒▐▒░░░▌\r\n" + //
+                                                "░░░░░▄▀▀▄░░░▄▄▀▀▒▒▒▒▌▒▒░░▌\r\n" + //
+                                                "░░░░▐▒░░░▀▄▀▒▒▒▒▒▒▒▒▒▒▒▒▒█\r\n" + //
+                                                "░░░░▌▒░░░░▒▀▄▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄\r\n" + //
+                                                "░░░░▐▒░░░░░▒▒▒▒▒▒▒▒▒▌▒▐▒▒▒▒▒▀▄\r\n" + //
+                                                "░░░░▌▀▄░░▒▒▒▒▒▒▒▒▐▒▒▒▌▒▌▒▄▄▒▒▐\r\n" + //
+                                                "░░░▌▌▒▒▀▒▒▒▒▒▒▒▒▒▒▐▒▒▒▒▒█▄█▌▒▒▌\r\n" + //
+                                                "░▄▀▒▐▒▒▒▒▒▒▒▒▒▒▒▄▀█▌▒▒▒▒▒▀▀▒▒▐░░░▄\r\n" + //
+                                                "▀▒▒▒▒▌▒▒▒▒▒▒▒▄▒▐███▌▄▒▒▒▒▒▒▒▄▀▀▀▀\r\n" + //
+                                                "▒▒▒▒▒▐▒▒▒▒▒▄▀▒▒▒▀▀▀▒▒▒▒▄█▀░░▒▌▀▀▄▄\r\n" + //
+                                                "▒▒▒▒▒▒█▒▄▄▀▒▒▒▒▒▒▒▒▒▒▒░░▐▒▀▄▀▄░░░░▀\r\n" + //
+                                                "▒▒▒▒▒▒▒█▒▒▒▒▒▒▒▒▒▄▒▒▒▒▄▀▒▒▒▌░░▀▄\r\n" + //
+                                                "▒▒▒▒▒▒▒▒▀▄▒▒▒▒▒▒▒▒▀▀▀▀▒▒▒▄▀",2);
+                    break;
+                default:
+                Mensageiro.exibirMensagem("Opção inválida. Tente novamente.\n",5);
+                    break;
+            }
+        } while (num1 != 3);
+        Mensageiro.exibirMensagem("\nVocê escolheu o Cesto. Encontrou um Gato.",90);
+                System.out.println("\n");
+        Mensageiro.exibirMensagem("Gato:  Ola eu, me chamo  Getters n' Setters",100);
+        Mensageiro.exibirMensagem("\nGato: Sou muito poderoso ",100);
+        Mensageiro.exibirMensagem("\nGato: Na verdade, Você já me conhce como... ",100);
+        Mensageiro.exibirMensagem("\nCausador da sua... ",100);
+        Mensageiro.exibirMensagem("\n ",900);
+        Mensageiro.exibirMensagem("SINUZITE ",240);
+        Mensageiro.exibirMensagem("\nGato: AIUHAIUHAIUHAIUHA MIAUUUU ",100);
+        Mensageiro.exibirMensagem("\n ",200);
+        Mensageiro.exibirMensagem("\nFIM ",200);
+        Mensageiro.exibirMensagem("\n",200);
+               
+        
+        scanner.close();  // Fechar o Scanner quando não for mais necessário
+    }
+}
