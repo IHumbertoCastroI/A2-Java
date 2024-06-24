2dad6386418c424b2644314005cb807f7320b509
Caio Labes <caiolabesdecastro@gmail.com>
2024-06-24 16:57:34 -0300

    Interfaces de Save/load e Render e Classe Personagem
    Criação das classes para salvar, carregar e renderizar as coisas do jogo e apenas a criação da classe do Personagem

---
+++ Saveable.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public interface Saveable  {
+    void salvar(BufferedWriter writer) throws IOException;
+    void carregar(BufferedReader reader) throws IOException;
+}
